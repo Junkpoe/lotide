5ec1d214fb50ea500b8eb1d021f3af552368e757
DevilGahlia <superman00897@hotmail.com>
2023-06-16 15:49:11 -0300

    lotide updoots

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log('✔✔✔Assertion Passed: [actual] === [expected]');
+  } else {
+    return console.log('🛑🛑🛑Assertion Failed: [Actual] !== [expected]');
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("The weather is nice", "The weather is nice");
+assertEqual("The chicken croseed the road", "To get to the other side");
+assertEqual(23, 2374);
+assertEqual(2745, 2745);
