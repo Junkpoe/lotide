622d6e64901c0991ca198f018f4e0a241c18f715
DevilGahlia <superman00897@hotmail.com>
2023-06-27 18:53:32 -0300

    Code to count letters ina string

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log('✔✔✔Assertion Passed: [actual] === [expected]');
+  } else {
+    return console.log('🛑🛑🛑Assertion Failed: [Actual] !== [expected]');
+  }
+};
+
+
+const letterCount = sentence => {
+  //Creating an object to store letter occurences
+  const countLetter = {};
+  //Looking to loop over each letter in a sentence
+  for (let x = 0; x < sentence.length; x++) {
+    const letter = sentence[x];
+    //Check if the letter is new or not and create a new property for it asigning it the value of 1
+    //added code to remove spaces so they won't be counted
+    if (letter === ' ') {
+
+      continue;
+
+    } else if (!countLetter[letter]) {
+      countLetter[letter] = 1;
+
+      //else we already have the letter and increment it by 1
+    } else {
+      countLetter[letter]++;
+    }
+  }
+
+  return countLetter;
+};
+
+console.log(letterCount("where do the chickens go"));
