9eb02bed71bba594e95d747bb8ab02f8d49ad51a
DevilGahlia <superman00897@hotmail.com>
2023-07-05 17:31:20 -0300

    Refactored code

--- assertEqual.js
+++ assertEqual.js
@@ -7,9 +7,5 @@ const assertEqual = function(actual, expected) {
 };
 
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("The weather is nice", "The weather is nice");
-assertEqual("The chicken croseed the road", "To get to the other side");
-assertEqual(23, 2374);
-assertEqual(2745, 2745);
+
+module.exports = assertEqual;
