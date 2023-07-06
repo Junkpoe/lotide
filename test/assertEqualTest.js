720c00222d6d3fc56ed6f50f2a4fcd89e9df4404
DevilGahlia <superman00897@hotmail.com>
2023-07-06 18:45:13 -0300

    Code to test tail

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("The weather is nice", "The weather is nice");
+assertEqual("The chicken croseed the road", "To get to the other side");
+assertEqual(23, 2374);
+assertEqual(2745, 2745);
