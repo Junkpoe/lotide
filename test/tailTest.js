6ce7201b1c3c677a890f83f50082b8acbe811e2e
DevilGahlia <superman00897@hotmail.com>
2023-07-06 19:01:49 -0300

    Refactored with chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,18 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(tail(['5']), '5');
+  });
+});
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-const num = [2];
-console.log(tail(num));
-const tiddle = [];
-console.log(tail(tiddle));
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(words));
+// const num = [2];
+// console.log(tail(num));
+// const tiddle = [];
+// console.log(tail(tiddle));
