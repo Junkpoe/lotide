ce5bc4b544c7a4e652416d764b0dce911209c1c6
DevilGahlia <superman00897@hotmail.com>
2023-07-06 18:46:06 -0300

    Test code for tail

---
+++ test/tailTest.js
@@ -0,0 +1,9 @@
+const tail = require('../tail');
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words));
+const num = [2];
+console.log(tail(num));
+const tiddle = [];
+console.log(tail(tiddle));
