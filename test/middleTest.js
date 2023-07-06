7a6d180681d9ac8ada0870a4679413a22479e67b
DevilGahlia <superman00897@hotmail.com>
2023-07-06 19:03:45 -0300

    Refactored with chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,16 @@
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 2);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(middle(['5']), '5');
+  });
+});
+
 //Code to test function
 
-console.log(middleArray([1]));
-console.log(middleArray(["Why", "did", "the", "chicken", "cross", "the", "road?"]))
+// console.log(middleArray([1]));
+// console.log(middleArray(["Why", "did", "the", "chicken", "cross", "the", "road?"]))
