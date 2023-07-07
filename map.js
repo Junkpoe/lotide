36cc257b2119d2c74ace0b9e942908303c78b901
DevilGahlia <superman00897@hotmail.com>
2023-07-06 22:30:19 -0300

    refactored

--- map.js
+++ map.js
@@ -1,18 +1,6 @@
-const eqArrays = function(arr1, arr2) {
-  for (let x = 0; x < arr1.length || x < arr2.length; ++x) {
-    if (arr1[x] !== arr2[x]) {
-      return false;
-    }
-  } return true;
-};
+const eqArrays = require('./eqArrays')
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual[0], actual[1]) !== expected) {
-    return false;
-  } else {
-    return true;
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const food = ["Chicken", "Pizza", "Fondue", "Teriyaki", "Sushi", "Ramen"];
 
@@ -24,6 +12,8 @@ const map = function(array, callback) {
   return results;
 };
 
-const resultsFood = map(food, word => word[0]);
-assertArraysEqual(resultsFood, ["C", "P", "F", "T", "S", "R"]);
+module.exports = map;
+
+// const resultsFood = map(food, word => word[0]);
+// assertArraysEqual(resultsFood, ["C", "P", "F", "T", "S", "R"]);
 
