ac1e0f93ac8b8b9a3fab04133f02ae105079c9c4
DevilGahlia <superman00897@hotmail.com>
2023-07-06 22:30:26 -0300

    refactored

--- takeUntil.js
+++ takeUntil.js
@@ -1,26 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log('✔✔✔Assertion Passed: [actual] === [expected]');
-  } else {
-    return console.log('🛑🛑🛑Assertion Failed: [Actual] !== [expected]');
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const eqArrays = function(arr1, arr2) {
-  for (let x = 0; x < arr1.length || x < arr2.length; ++x) {
-    if (arr1[x] !== arr2[x]) {
-      return false;
-    }
-  } return true;
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual[0], actual[1]) !== expected) {
-    return false;
-  } else {
-    return true;
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const takeUntil = function(array, callback) {
   let result = [];
@@ -33,20 +15,22 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+module.exports = takeUntil;
 
-console.log('---');
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
 
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+// console.log('---');
 
-const test = ["This", "is", "a", "test", ".", "Here's", "a", "second", "test"];
-const result1 = takeUntil(test, x => x === '.');
-console.log(result1);
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
 
-assertArraysEqual(results1, data1.slice(0, 5));
-assertArraysEqual(results2, data2.slice(0, 4));
-assertArraysEqual(result1, test.slice(0, 4));
+// const test = ["This", "is", "a", "test", ".", "Here's", "a", "second", "test"];
+// const result1 = takeUntil(test, x => x === '.');
+// console.log(result1);
+
+// assertArraysEqual(results1, data1.slice(0, 5));
+// assertArraysEqual(results2, data2.slice(0, 4));
+// assertArraysEqual(result1, test.slice(0, 4));
